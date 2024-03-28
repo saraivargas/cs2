5a145a970a2dfa4ce19b2f44490e45612afd98ea
Sarai Vargas <vargas1sarai@gmail.com>
2024-03-27 20:21:48 -0700

    start bar

--- mywebsite/src/App.jsx
+++ mywebsite/src/App.jsx
@@ -3,35 +3,20 @@ import { useState } from "react";
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline text-center text-red-500">
-        Hello world!
-      </h1>
-      <MyButton />
-      <div className="flex">
-        <MyPizza />
-        <MyPizza />
+      {/* top bar */}
+      <div className="flex justify-between items-center px-4 bg-slate-950 text-white border-b-2 border-red-500">
+        <div className="w-25">Sarai</div>
+        <div className="w-60">Search Bar</div>
+        <div class="grid grid-cols-4 gap-4 py-2 w-2/5">
+          <div class="h-10 bg-slate-700 rounded-md flex justify-center items-center">Home</div>
+          <div class="h-10 bg-slate-700 rounded-md flex justify-center items-center">Favorites</div>
+          <div class="h-10 bg-slate-700 rounded-md flex justify-center items-center">Profile</div>
+          <div class="h-10 bg-slate-700 rounded-md flex justify-center items-center">Settings</div>
+        </div>
       </div>
+      <h1 className="text-3xl font-bold underline text-center text-red-500">Hello world!</h1>
     </div>
   );
-}
-
-function MyPizza() {
-  return (
-    <div>
-      <img
-        src={
-          "https://thumbs.dreamstime.com/b/pizza-slice-above-cartoon-illustration-vector-pepperoni-paprika-mushroom-cheese-tomato-sauce-white-background-135026080.jpg"
-        }
-        width={300}
-        height={300}
-      />
-      <p className="text-center pl-2">I love pizza!</p>
-    </div>
-  );
-}
-
-function MyButton() {
-  return <button>I'm a button</button>;
 }
 
 export default App;
---
+++ mywebsite/src/Pizza.jsx
@@ -0,0 +1,14 @@
+function MyPizza() {
+  return (
+    <div>
+      <img
+        src={
+          "https://thumbs.dreamstime.com/b/pizza-slice-above-cartoon-illustration-vector-pepperoni-paprika-mushroom-cheese-tomato-sauce-white-background-135026080.jpg"
+        }
+        width={300}
+        height={300}
+      />
+      <p className="text-center pl-2">I love pizza!</p>
+    </div>
+  );
+}
